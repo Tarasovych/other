db58b4762a3c3878800ec90edeea3cbe0a486fd9
taras <tarasovych.ua@gmail.com>
2017-02-14 14:52:59 +0200

    1

---
+++ oop/oop1.php
@@ -0,0 +1,36 @@
+<?php
+
+class Worker 
+{
+
+	// public $name = 'ВАСЯ';
+	// public $age = 10;
+	// public $salary = 1000;
+
+ 	public function get()
+ 	{
+ 		return $this->name."<br/>".$this->age." років<br/>".$this->salary." $<br/>";
+ 	}
+
+ 	public function __construct($newName,$newAge,$newSalary)
+ 	{
+ 		$this->name = $newName;
+ 		$this->age = $newAge;
+ 		$this->salary = $newSalary;
+ 	}
+
+}
+
+$new_object_Worker = new Worker("ВАСЯ",10,1000);
+$new_object_Worker_2 = new Worker("ІВАН",30,2000);
+
+echo $new_object_Worker->get() . "<br/>";
+
+// $new_object_Worker_2->set("ІВАН",30,2000) . "<br/>";
+
+echo $new_object_Worker_2->get() . "<br/>";
+
+echo "загальний вік " . ($new_object_Worker->age + $new_object_Worker_2->age) . "<br/>";
+echo "загальна з/п " . ($new_object_Worker->salary + $new_object_Worker_2->salary . " $");
+
+?>
